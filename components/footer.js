bd3a22e5ec684a63b7288f95a66809d72edd51c1
Desarrollos-Web-Urquiza <desarrollos_web_urquiza@hotmail.com>
2021-06-10 02:13:12 -0300

    Mejorando estilos del Footer

--- components/footer.js
+++ components/footer.js
@@ -7,7 +7,8 @@ const Footer = ({coditionalClass}) => {
   console.log(coditionalClass)
   return(
     <>
-      <div className={`${styles.footer}`} >
+      <div className={utilStyles.patchFooter}  >
+      <div style={{marginTop: 120}}  className={`${styles.footer}`} >
         <div  className={`${styles.footerContent} ${utilStyles.sectionFooter}`}>
           <div className={styles.container} >
             <div className={`${utilStyles.dF} ${utilStyles.mb20}`}>
@@ -21,7 +22,6 @@ const Footer = ({coditionalClass}) => {
           </div>
         </div>
       </div>
-      <div className={utilStyles.patchFooter}  >
       </div>
     </>
   )
